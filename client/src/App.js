946ed6a3295f097ffee759d22fce4e8128fb6e73
Paulo Camargo <camargo.paulo@outlook.com>
2022-07-15 14:49:56 -0300

    cont

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios';
+import Card from './components/cards/card.js'
 
 function App() {
   const [values, setValues] = useState();
+  const [listUsers, allUsers] = useState();
 
   const handleChangedValues = (value) => {
     setValues((prevValue) => ({
@@ -12,31 +15,45 @@ function App() {
   };
 
   const handleClickButton = () => {
-    console.log(values);
+    Axios.post("http://localhost:3001/register", {
+      name: values.name,
+      surname: values.surname,
+    }).then((response) => {
+      console.log(response);
+    });
   };
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getcard").then((response) => {
+    allUsers(response.data);
+  });
+  },[]);
 
   return (
     <div className="app-container">
       <div className='register-container'>
         <h1>Registro Vileco</h1>
-        <input 
-        type='text'
-        name='name'
-        placeholder='Nome'
-        className='register-input'
-        onChange={handleChangedValues}
+        <input
+          type='text'
+          name='name'
+          placeholder='Nome'
+          className='register-input'
+          onChange={handleChangedValues}
         />
-        <input 
-        type='text'
-        name='surname'
-        placeholder='Sobrenome'
-        className='register-input'
-        onChange={handleChangedValues}
+        <input
+          type='text'
+          name='surname'
+          placeholder='Sobrenome'
+          className='register-input'
+          onChange={handleChangedValues}
         />
         <button className='register-input' onClick={() => handleClickButton()}>Cadastrar</button>
       </div>
+      {typeof listUsers !== "undefined" && listUsers.map((value) => {
+        return <Card key={value.id}></Card>
+      })}
     </div>
   );
-}
+};
 
 export default App;
